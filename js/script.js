5d116e238bf507e49dd70a5fe716a15e351d0d94
Giorgio <shadigiorgioislam@gmail.com>
2021-01-25 16:00:38 +0100

    Add image input

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@ var app = new Vue({
   el: '#root',
   data: {
     nome:'',
-    cognome:''
+    cognome:'',
+    src: 'https://wallpaperaccess.com/full/4584342.png',
+    titolo: 'Vue',
+    show: '',
+    text: 'Apri immagine'
+  },
+  methods: {
+    getImage(){
+      if(this.show == 'active'){
+        this.show = ''
+        this.text = 'Apri immagine'
+      }
+      else{
+        this.show = 'active';
+        this.text = 'Chiudi immagine'
+      }
+    }
   }
 })
